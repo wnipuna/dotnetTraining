3aaceda9008805e380b0bc050cb3d32734c6a4a3
wnipuna <36652803+wnipuna@users.noreply.github.com>
2025-12-03 22:44:48 +0530

    Add files via upload

---
+++ day3/ConsoleAppOop/ConsoleAppOop/GenericRepository.cs
@@ -0,0 +1,14 @@
+﻿public class GenericRepository<T>
+{
+    private List<T> Items = new List<T>();
+
+    public void Add(T item)
+    {
+        Items.Add(item);
+    }
+
+    public List<T> GetAll()
+    {
+        return Items;
+    }
+}
---
+++ day3/ConsoleAppOop/ConsoleAppOop/Program.cs
@@ -0,0 +1,27 @@
+﻿IEmployeeService employeeService = new EmployeeService();
+
+Employee Employee = new Employee { GetId = 1, GetName = "Nipuna", GetDepartment = "SE" };
+Manager manager = new Manager { GetId = 2, GetName = "Ruwantha", GetDepartment = "IT", TeamSize = 5 };
+
+employeeService.AddEmployee(Employee);
+employeeService.AddEmployee(manager);
+
+Console.WriteLine("All Employees:");
+foreach (var emp in employeeService.GetAllEmployees())
+{
+    emp.DisplayDetails();
+}
+
+Console.WriteLine("\nGet Employee by ID (2):");
+var found = employeeService.GetEmployeeById(2);
+found?.DisplayDetails();
+
+GenericRepository<Employee> EmployeeRepo = new GenericRepository<Employee>();
+EmployeeRepo.Add(Employee);
+EmployeeRepo.Add(manager);
+
+Console.WriteLine("\nGeneric Repository contents:");
+foreach (var e in EmployeeRepo.GetAll())
+{
+    e.DisplayDetails();
+}

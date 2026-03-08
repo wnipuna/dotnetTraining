c48bbcb3f91bf56fe3c12677f4cf91d8f0051d39
wnipuna <36652803+wnipuna@users.noreply.github.com>
2026-03-08 16:56:53 +0530

    day 7 linq

---
+++ day7/EFproject/EFproject/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFproject
+{
+    internal class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+    }
+}
---
+++ day7/EFproject/EFproject/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFproject
+{
+    class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+        // Foreign key for Department
+        public int DepartmentId { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        public Department Department { get; set; } = null!;
+
+    }
+}
